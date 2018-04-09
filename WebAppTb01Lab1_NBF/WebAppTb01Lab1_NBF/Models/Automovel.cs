81d3b938c7bd36a39db15af222e136fc5b680991
NeemiasB <36780438+NeemiasB@users.noreply.github.com>
2018-04-09 16:49:26 -0400

    Add files via upload

---
+++ WebAppTb01Lab1_NBF/WebAppTb01Lab1_NBF/Models/Automovel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTb01Lab1_NBF.Models
+{
+    public class Automovel
+    {
+        public int AutomovelId { get; set; }
+        [Required(ErrorMessage = "Tipo é obrigatorio")]
+
+        public Tipo Tipo { get; set; }
+        [Required(ErrorMessage = "Descrição é obrigatorio")]
+        [StringLength(300, ErrorMessage = "Maximo 300 caracteres")]
+
+        public String Descricao { get; set; }
+        [Required(ErrorMessage = "Marca é obrigatorio")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+
+        public String Marca { get; set; }
+        [Required(ErrorMessage = "Disponivel é obrigatorio")]
+
+        public bool Disponivel { get; set; }
+        [Required(ErrorMessage = "DataCadastro é obrigatorio")]
+
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ WebAppTb01Lab1_NBF/WebAppTb01Lab1_NBF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppTb01Lab1_NBF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
